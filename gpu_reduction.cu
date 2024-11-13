843a94fd30e8bb9bd121c2c108dbd05d5666cf56
Glenvelis <gperez42@smith.edu>
2024-11-13 16:31:51 -0500

    very faulty sum operation in GPU Reduction

--- gpu_reduction.cu
+++ gpu_reduction.cu
@@ -5,7 +5,9 @@
 
 #define SIZE 50
 
-int Reduction(int* input, int* output, int operation){
+//  multi-kernel for being able to handle an array larger than 2048
+
+__global__ void Reduction(int* input, int* output, int operation){
 	// Stride is distance to the next value being
 	// accumulated into the threads mapped position
 	// in the partialSum[] aray
@@ -19,31 +21,55 @@ int Reduction(int* input, int* output, int operation){
 	}
 	*/
 
+/*
 	// Sum Operation
 	if (operation == 0){
-		for (unsigned int stride = 1; stride <= SIZE; stride *= 2) {
+		for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2) {
 			if (SIZE % stride == 0){
-				// output[stride] = input[stride] + input[stride*2];
+				//output[stride] = input[stride] + input[stride*2];
+				//output[2*SIZE] = input[2*SIZE] + input[2*SIZE*stride];
 				output[2*SIZE] = input[2*SIZE] + input[2*SIZE*stride];
+
 			}
 				
 		}
+	printf("Sum: %d\n", output[SIZE]);
 	// printf("Output: %d", output[stride]);
 	}	
+	//return output[SIZE];
+*/
+
+
+	if (operation == 0) {
+		for (unsigned int stride = blockDim.x; stride >= 1; stride /= 2) {
+		 	
+		 __syncthreads();
+		 
+			 if (SIZE < stride) {
+		 		output[SIZE] += output[SIZE+stride];
+		 	}
+		}	
+	printf("Sum: %d\n", output[SIZE]);
+	
+	}
+
+
 }
 
 int main(){
 	// allocate memory
-	int* input = (int*) malloc(sizeof(int) * SIZE);
-	int* output = (int*) malloc(sizeof(int) * SIZE);
+	int *input, *output; 
+	cudaMallocManaged(&input, sizeof(int) * SIZE);
+	cudaMallocManaged(&output, sizeof(int) * SIZE);	
 
   	// initialize inputs
   	for (int i = 0; i < SIZE; i++) {
   		input[i] = 1;
    	}
 	
-	//Reduction(input, output, 0);
-	printf("Output: %d", Reduction(input, output, 0));
+	// Reduction(input, output, 0);
+	// printf("Output: %d", Reduction(input, output, 0));
+	Reduction<<<1, SIZE>>>(input, output, 0);
 
 	/*
 	  // check results
@@ -52,7 +78,9 @@ int main(){
   	 }
  	 printf("\n");
  	*/ 
-	
+
+	cudaFree(input);
+	cudaFree(output);
 
 	return 0;
 	
