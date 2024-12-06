063f2f2e59ec29848646d27676f34de81c1c8a27
Glenvelis <gperez42@smith.edu>
2024-12-06 14:15:12 -0500

    gpu reduction sum working for elements less than or equal to 2048

--- gpu_reduction.cu
+++ gpu_reduction.cu
@@ -6,71 +6,37 @@
 #define SIZE 50
 
 //  multi-kernel for being able to handle an array larger than 2048
+// gpu can't return from the kernel
+__global__ void Reduction(int* input, int operation){
 
-__global__ void Reduction(int* input, int* output, int operation){
-	// Stride is distance to the next value being
-	// accumulated into the threads mapped position
-	// in the partialSum[] aray
-	/*
-	for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2) {
-		 __syncthreads();
-		
-		 if (t % stride == 0) {
-			 partialSum[2*t]+= partialSum[2*t+stride];
-		}
-	}
-	*/
-
-/*
-	// Sum Operation
-	if (operation == 0){
-		for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2) {
-			if (SIZE % stride == 0){
-				//output[stride] = input[stride] + input[stride*2];
-				//output[2*SIZE] = input[2*SIZE] + input[2*SIZE*stride];
-				output[2*SIZE] = input[2*SIZE] + input[2*SIZE*stride];
-
-			}
-				
-		}
-	printf("Sum: %d\n", output[SIZE]);
-	// printf("Output: %d", output[stride]);
-	}	
-	//return output[SIZE];
-*/
-
-
+// Sum Operation
 	if (operation == 0) {
-		output[SIZE] = 0;
-		for (unsigned int stride = blockDim.x; stride >= 1; stride /= 2) {
+		for (unsigned int stride = 1;  stride <= blockDim.x; stride *= 2) {
 		 	
 		 __syncthreads();
 
-	
-			if (SIZE < stride) {
-		 		output[SIZE] += input[SIZE+stride];
+			
+		 	if (threadIdx.x % stride == 0) {
+		 		input[2*threadIdx.x] += input[2*threadIdx.x+stride];
 		 	}
-		 
-		 /*
-		 	if (threadIdx.x < stride) {
-		 		output[threadIdx.x] += output[threadIdx.x+stride];
-		 	}
-		 */		
+		 		
 		}	
-	
-	printf("Sum: %d\n", output[SIZE]);
+
+	if (threadIdx.x == 0) {
+		printf("Sum: %d\n", input[0]);
+	}
 	
 	}
-
 
 }
 
 int main(){
 	// allocate memory
-	int *input, *output; 
-	// int sum;	
+	// int *input, *output; 
+	int *input;
+	
 	cudaMallocManaged(&input, sizeof(int) * SIZE);
-	cudaMallocManaged(&output, sizeof(int));
+	// cudaMallocManaged(&output, sizeof(int) * SIZE);
 
   	// initialize inputs
   	for (int i = 0; i < SIZE; i++) {
@@ -79,7 +45,19 @@ int main(){
 	
 	// Reduction(input, output, 0);
 	// printf("Output: %d", Reduction(input, output, 0));
-	Reduction<<<1, SIZE>>>(input, output, 0);
+
+	Reduction<<<1, SIZE>>>(input, 0);
+
+/*
+	if SIZE < 2048{
+		Reduction<<<1, SIZE>>>(input, output, 0);
+	}
+	
+	else{
+		Reduction<<<1, SIZE>>>(input, output, 0);
+		// ReductionLarge<<<>>>();
+	}
+*/
 
 	/*
 	  // check results
@@ -91,7 +69,7 @@ int main(){
  	printf("%s\n", cudaGetErrorString(cudaGetLastError()));
 
 	cudaFree(input);
-	cudaFree(output);
+	// cudaFree(output);
 
 	return 0;
 	
